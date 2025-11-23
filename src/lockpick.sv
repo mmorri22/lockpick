99f924150313f9d25833f33ce2d9798ec6d0c7c7
mmorri22 <48807588+mmorri22@users.noreply.github.com>
2025-11-23 15:59:58 -0500

    Update lockpick.sv

--- src/lockpick.sv
+++ src/lockpick.sv
@@ -232,7 +232,7 @@ endmodule
 module tt_um_mmorri22_lockpick_game (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [1:0] uio_in,   // IOs: Input path
+    input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
